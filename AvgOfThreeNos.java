56afdd3c03a4ff17f14a24235dd8aa36658e2507
Samarth <csamarth2005@gmail.com>
2026-06-02 19:37:00 +0530

    Avg of three nos

---
+++ AvgOfThreeNos.java
@@ -0,0 +1,9 @@
+public class AvgOfThreeNos {
+    public static void main(String[] args) {
+        int a = 67;
+        int b = 89;
+        int c = 90;
+        int avg = (a+b+c) / 3;
+        System.out.print("Average :"+avg);
+    }
+}
