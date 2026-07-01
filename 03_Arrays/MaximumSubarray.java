b1d2ec3e826a414261b05036b7ddfdc581e5a291
Samarth <csamarth2005@gmail.com>
2026-07-01 11:08:15 +0530

    updated kadanes algo

--- 03_Arrays/MaximumSubarray.java
+++ 03_Arrays/MaximumSubarray.java
@@ -21,6 +21,8 @@ public class MaximumSubarray {
         return maxi;
     }
     public static void main(String[] args) {
-        
+        int nums[] = {7,8,-9,1,-3,4,-5,6};
+        int ans = maxSubarray(nums);
+        System.out.println(ans);
     }
 }
